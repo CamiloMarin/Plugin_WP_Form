3abaafe4d612edb59c5d6cb483c15c24e34e6a0c
CamiloMarin <camilomv1234@gmail.com>
2025-01-14 22:35:59 -0500

    Creación del archivo principal y de php del proyecto
    Primera vez creando un archivo correspondiente a un plugin de Wordpres.
    
    V0.1

---
+++ form-page-view.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: test-plugin
+* Plugin URI: https://www.tu-sitio.com/
+* Description: Test.
+* Version: 0.1
+* Author: tu-nombre
+* Author URI: https://www.tu-sitio.com/
+**/
